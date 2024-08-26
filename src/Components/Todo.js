d1f7c29a09e2c3189fbf72476a007c898e5d7ee7
siva-245711 <sivanagendra809@gmail.com>
2024-08-26 22:07:29 +0530

    css

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,48 +1,59 @@
-import './Css/Todo.css'
-import { useState} from 'react';
-import { useRef } from 'react';
-import { useEffect } from 'react'; 
+import './Css/Todo.css';
+import { useState, useRef, useEffect } from 'react';
 import Todoitems from './Todoitems';
 let count = 0;
- const Todo = () => {
+
+const Todo = () => {
   const [todos, setTodos] = useState([]);
   const inputRef = useRef(null);
-  
-  
+
   const add = () => {
-    setTodos([...todos,{no:count++,text:inputRef.current.value,display:" "}]);
+    setTodos([...todos, { no: count++, text: inputRef.current.value, display: " " }]);
     inputRef.current.value = "";
-    localStorage.setItem("todos_count",count);
+    localStorage.setItem("todos_count", count);
+
+    // Trigger background pulse animation
+    const todoElement = document.querySelector('.todo');
+    todoElement.style.animation = 'none'; // Reset animation
+    setTimeout(() => {
+      todoElement.style.animation = ''; // Reapply animation
+    }, 10);
   }
-  useEffect(() => {
-    setTodos(JSON.parse(localStorage.getItem("todos"))) ; 
-    count = localStorage.getItem("todos_count");
-  },[])
 
   useEffect(() => {
-   
+    setTodos(JSON.parse(localStorage.getItem("todos")));
+    count = localStorage.getItem("todos_count");
+  }, []);
+
+  useEffect(() => {
     setTimeout(() => {
       console.log(todos);
-      localStorage.setItem("todos",JSON.stringify(todos));
-    },100)
-  }, [todos])
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }, 100);
+  }, [todos]);
 
-  
+  useEffect(() => {
+    const todoList = document.querySelector('.todo-list');
+    todoList.scrollTo({
+      top: todoList.scrollHeight,
+      behavior: 'smooth'
+    });
+  }, [todos]);
+
   return (
     <div className='todo'>
       <div className="todo-header">TO-DO-LIST</div>
       <div className="todo-add">
-        <input ref={inputRef} type="text" placeholder='Add Your Task'className='todo-input' />
-          <div onClick={()=>{add()}} className='todo-add-btn'>ADD</div>
+        <input ref={inputRef} type="text" placeholder='Add Your Task' className='todo-input' />
+        <div onClick={add} className='todo-add-btn'>ADD</div>
       </div>
-        <div className="todo-list">
-          {todos.map((item,index)=>{
-            return < Todoitems key={index} setTodos={setTodos} no={item.no}display={item.display} text={item.text} />
-
-          })}
-        </div>
-
+      <div className="todo-list">
+        {todos.map((item, index) => {
+          return <Todoitems key={index} setTodos={setTodos} no={item.no} display={item.display} text={item.text} />;
+        })}
+      </div>
     </div>
-  )
+  );
 }
+
 export default Todo;
